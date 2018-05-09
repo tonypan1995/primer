0ab73e9a8650ed203fdb5519849d3cd3914c95fa
tonypan1995 <1045380922@qq.com>
2018-05-08 21:18:13 -0400

    create_ch3_1

---
+++ ch3/ch3_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::string;
+using std::cin; using std::cout; using std::endl;
+int main() {
+    // 3.2.2
+    // string s;
+    // cin >> s;
+    // cout << s << endl;
+    
+    string word;
+    while (cin >> word) {
+        cout << word << endl;
+    }
+    return 0;
+}
